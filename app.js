c151be36190d5489f9f5155c3a9348809390e51a
philip <pmutua@live.com>
2020-08-20 11:45:32 +0300

    feat: Added endpoint to retrieve safaricom's daraja access token

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const  request = require('request');
 const app = express();
 
+require('dotenv').config()
+
+// console.log(process.env);
 
 
 
@@ -9,7 +13,30 @@ app.get('/', (re,res) =>{
     res.send("Test")
 })
 
-
+app.get('access_token', (req,res) =>{
+    // access token 
+    consumer_key = process.env.YOUR_APP_CONSUMER_KEY,
+    consumer_secret = process.env.YOUR_APP_CONSUMER_SECRET,
+    url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+    auth = "Basic " + new Buffer(consumer_key + ":" + consumer_secret).toString("base64");
+  
+    request(
+      {
+        url : url,
+        headers : {
+          "Authorization" : auth
+        }
+      },
+      function (error, response, body) {
+        // TODO: Use the body object to extract OAuth access token
+        if (error){
+            console.log(error)
+        }else{
+            res.status(200).json(body)
+        }
+      }
+    )
+})
 
 
 
@@ -17,4 +44,9 @@ app.get('/', (re,res) =>{
 
 
 //listen 
-app.listen(3000);
+app.listen(3000, (err,live) => {
+    if(err){
+        console.log(err)
+    }
+    console.log("Server running on port 3000")
+});
