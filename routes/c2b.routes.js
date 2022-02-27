038f3ff66e4beebef487391939a98b522543f77b
pmutua <pmutua@live.com>
2022-02-27 19:05:01 +0300

    Added env variables for c2b route

--- routes/c2b.routes.js
+++ routes/c2b.routes.js
@@ -1,59 +1,43 @@
 const router = require('express').Router()
 
 const C2b = require('../models/c2B.model');
+const mpesaAuth = require('../middlewares/mpesaAuth.middleware');
 
 
-router.route('/').get((req,res)=>{
-    C2b.find()
+router.route('/').get((req, res) => {
+  C2b.find()
     .then(transactions => res.json(transactions))
-    .catch(err => res.status(400).json('Error: '+err));
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
-// router.route('/register').post((req,res)=>{
-//     const payload = req.body;
-//     const newC2b = new C2b(payload);
-
-//     newC2b.save()
-//     .then(()=> res.json('c2b success!'))
-//     .catch(err=> res.status(400).json('Error: '+ err));
-// });
-
-
-
 // register c2b
-router.route('/register').get(mpesaAuth, (req,res)=>{
-    const url = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
-    const auth = "Bearer"+" "+req.access_token
-    request(
-      {
-        url,
-        method: "POST",
-        headers: {
-          "Authorization": auth
-        },
-        json: {
-            "ShortCode": "600982",
-            "ResponseType": "Completed",
-            "ConfirmationURL": "http://192.168.0.102:3000/confirmation",
-            "ValidationURL": "http://192.168.0.102:3000/validation"
+router.route('/register').get(mpesaAuth, (req, res) => {
+  const url = process.env.C2B_REGISTER_URL
+  const auth = "Bearer" + " " + req.access_token
+  const confirmationUrl = process.env.C2B_CONFIRMATION_URL
+  const validationUrl = process.env.C2B_VALIDATION_URL
+  const c2bShortCode = process.env.C2B_SHORT_CODE
 
-        }
+  request(
+    {
+      url,
+      method: "POST",
+      headers: {
+        "Authorization": auth
       },
-      function(error,response,body){
-        if(error){console.log(error)}
-        // console.log(response.body)
-        res.status(200).json(body)
-
-        // response
-
-        // {
-        //   OriginatorCoversationID: '7603-31746121-1',
-        //   ResponseCode: '0',
-        //   ResponseDescription: 'success'
-        // }
+      json: {
+        "ShortCode": `${c2bShortCode}`,
+        "ResponseType": "Completed",
+        "ConfirmationURL": `${confirmationUrl}`,
+        "ValidationURL": `${validationUrl}`
 
       }
-    )
+    },
+    function (error, response, body) {
+      if (error) { console.log(error) }
+      res.status(200).json(body)
+    }
+  )
 
 });
 
