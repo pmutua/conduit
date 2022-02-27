f7d5c6dddbbb0e6cd78b1d1403b0d12523690ae0
pmutua <pmutua@live.com>
2022-02-27 20:38:17 +0300

    Added DARAJA_ACCESS_TOKEN_URL to environment variables

--- middlewares/mpesaAuth.middleware.js
+++ middlewares/mpesaAuth.middleware.js
@@ -3,7 +3,7 @@ const request = require("request");
 module.exports = function getDarajaAccessToken(req,res,next){
     consumer_key = process.env.YOUR_APP_CONSUMER_KEY,
     consumer_secret = process.env.YOUR_APP_CONSUMER_SECRET,
-    url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+    url = process.env.DARAJA_ACCESS_TOKEN_URL
     auth = "Basic " + Buffer.from(consumer_key + ":" + consumer_secret,'utf-8').toString("base64");
   
     request(
