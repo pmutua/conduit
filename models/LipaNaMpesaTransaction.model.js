6c8be569e96f14f2087ff81ee8748e3680603090
pmutua <pmutua@live.com>
2022-02-27 20:50:18 +0300

    chore: removed the json reference code

--- models/LipaNaMpesaTransaction.model.js
+++ models/LipaNaMpesaTransaction.model.js
@@ -23,41 +23,10 @@ const lipaNaMpesaTransactionSchema = new Schema({
         required: true,
     },
 },
-{
-    timestamps:true
-});
+    {
+        timestamps: true
+    });
 
 
 
-module.exports = mongoose.model('LipaNaMpesaTransaction',lipaNaMpesaTransactionSchema)
-
-
-
-// {    
-//     "Body": {        
-//        "stkCallback": {            
-//           "MerchantRequestID": "29115-34620561-1",            
-//           "CheckoutRequestID": "ws_CO_191220191020363925",            
-//           "ResultCode": 0,            
-//           "ResultDesc": "The service request is processed successfully.",            
-//           "CallbackMetadata": {                
-//              "Item": [{                        
-//                 "Name": "Amount",                        
-//                 "Value": 1.00                    
-//              },                    
-//              {                        
-//                 "Name": "MpesaReceiptNumber",                        
-//                 "Value": "NLJ7RT61SV"                    
-//              },                    
-//              {                        
-//                 "Name": "TransactionDate",                        
-//                 "Value": 20191219102115                    
-//              },                    
-//              {                        
-//                 "Name": "PhoneNumber",                        
-//                 "Value": 254708374149                    
-//              }]            
-//           }        
-//        }    
-//     }
-//  }
+module.exports = mongoose.model('LipaNaMpesaTransaction', lipaNaMpesaTransactionSchema)
