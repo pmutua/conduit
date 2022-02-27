8fe51395cfb90b35839383ca23af0971b926bc85
pmutua <pmutua@live.com>
2022-02-27 19:04:44 +0300

    Added env variables for Lipa na mpesa route

--- routes/LipaNaMpesa.route.js
+++ routes/LipaNaMpesa.route.js
@@ -7,62 +7,63 @@ const mpesaAuth = require('../middlewares/mpesaAuth.middleware');
 
 
 
-router.route('/').get((req,res)=>{
-    LipaNaMpesaTransaction.find()
+router.route('/').get((req, res) => {
+  LipaNaMpesaTransaction.find()
     .then(transactions => res.json(transactions))
-    .catch(err => res.status(400).json('Error: '+err));
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/callback').post((req,res)=>{
-    console.log(req.body)
-    const transaction = req.body;
-   
-    const newTransaction = new LipaNaMpesaTransaction(transaction);
+router.route('/callback').post((req, res) => {
+  console.log(req.body)
+  const transaction = req.body;
+  const newTransaction = new LipaNaMpesaTransaction(transaction);
 
-    newTransaction.save()
-    .then(()=> res.json('User added!'))
-    .catch(err=> res.status(400).json('Error: '+ err));
+  newTransaction.save()
+    .then(() => res.json('User added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 
 
-router.route('/stk').post(mpesaAuth, (req,res)=>{
-    const url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
-    const auth = "Bearer "+ req.access_token
-    const _shortCode = "174379"
-    const _passKey = process.env.MPESA_PASS_KEY
-    const timeStamp = (new Date()).toISOString().replace(/[^0-9]/g, '').slice(0, -3)
-    const password = Buffer.from(`${_shortCode}${_passKey}${timeStamp}`).toString('base64')
-  
-    request(
-      {
-        url,
-        method: "POST",
-        headers:{
-          Authorization: auth
-        },
-        json:{
-          "BusinessShortCode":"174379",
-          "Password": password,
-          "Timestamp":timeStamp,
-          "TransactionType": "CustomerPayBillOnline",
-          "Amount":`${req.body.Amount}`,
-          "PartyA":`${req.body.PhoneNumber}`,
-          "PartyB":174379,
-          "PhoneNumber":`${req.body.PhoneNumber}`,
-          "CallBackURL":"https://b151-197-232-106-157.ngrok.io/lipa_na_mpesa/callback",
-          "AccountReference":`${req.body.AccountReference}`,
-          "TransactionDesc":`${req.body.TransactionDesc}`
-  
-        }
+router.route('/stk').post(mpesaAuth, (req, res) => {
+  const url = process.env.LIPA_NA_MPESA_STK_PUSH_REQUEST_URL
+  const auth = "Bearer " + req.access_token
+  const _shortCode = process.env.LIPA_NA_MPESA_SHORT_CODE
+  const _passKey = process.env.MPESA_PASS_KEY
+  const timeStamp = (new Date()).toISOString().replace(/[^0-9]/g, '').slice(0, -3)
+  const password = Buffer.from(`${_shortCode}${_passKey}${timeStamp}`).toString('base64')
+  const partyB = process.env.LIPA_NA_MPESA_PARTYB
+  const callBackUrl = process.env.LIPA_NA_MPESA_CALLBACK_URL
+
+  request(
+    {
+      url,
+      method: "POST",
+      headers: {
+        Authorization: auth
       },
-      function(error,response,body){
-        if(error){res.status(400).json(error)}
-        res.status(200).json(body)
+      json: {
+        "BusinessShortCode": `${_shortCode}`,
+        "Password": password,
+        "Timestamp": timeStamp,
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": `${req.body.Amount}`,
+        "PartyA": `${req.body.PhoneNumber}`,
+        "PartyB": `${partyB}`,
+        "PhoneNumber": `${req.body.PhoneNumber}`,
+        "CallBackURL": `${callBackUrl}`,
+        "AccountReference": `${req.body.AccountReference}`,
+        "TransactionDesc": `${req.body.TransactionDesc}`
 
       }
-  
-    )
-  })
+    },
+    function (error, response, body) {
+      if (error) { res.status(400).json(error) }
+      res.status(200).json(body)
+
+    }
+
+  )
+})
 
 module.exports = router;
