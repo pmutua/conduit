f73000730d027f814db64e0cedebaab8b3f78841
pmutua <pmutua@live.com>
2022-02-27 20:37:46 +0300

    Comments removed

--- loaders/router.loader.js
+++ loaders/router.loader.js
@@ -8,8 +8,6 @@ var lipaNaMpesaRouter = require("../routes/LipaNaMpesa.route");
 module.exports = function(app) {
   app.use(express.json());
 
-//   app.use("/", indexRouter);
-//   app.use("/router1", router1Router);
   app.use("/transactions", transactionsRouter);
   app.use("/c2b", c2bRouter);
   app.use("/lipa_na_mpesa/", lipaNaMpesaRouter);
